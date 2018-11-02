2dd2159864fcfa51d133f3924dc991fbef17368e
DianaSensei <43933486+thong144@users.noreply.github.com>
2018-11-02 22:00:30 +0700

    update 10:00PM 3-11

--- src/QFloat.cpp
+++ src/QFloat.cpp
@@ -14,6 +14,7 @@ bool* str2Bin(string s) {
 	// create bool to contain
 	bool*a = new bool[bit];
 
+	int exponent = -1;
 	int startPos = 0;
 
 	if (s[0] == '-')
@@ -28,12 +29,40 @@ bool* str2Bin(string s) {
 		if(s[i]=='.'){
 			pointPos = i; break;
 		}
-
+	// vd -123.567
+	// string int to bin
 	string intStr;
 	intStr = s.substr(startPos, pointPos - startPos);
+	string boolInt;
+	string sample(intStr.length(), '0');
+	while (intStr != sample) {
+		boolInt.push_back((intStr[intStr.length()] - '0') % 2);
+		intStr = strDiv2(intStr);
+	}
+	boolInt.reserve();
+	pointPos = boolInt.length();
 
-
-
+	// string frac to bin
 	string fracStr;
 
+
+
+	// find exponent
+	int index = 0;
+	while (exponent == -1) {
+		if (boolInt[index] == 1) {
+			exponent = pointPos-index-1;
+		}
+		index++;
+	}
+
+
+}
+string strDiv2(string s) {
+	string substr; bool mem = 0;
+	for (int i = 0; i < s.size(); i++) {
+		substr.push_back(((mem * 10 + (s[i] - '0')) / 2) + '0');
+		mem = (mem * 10 + (s[i] - '0')) % 2;
+	}
+	return substr;
 }
