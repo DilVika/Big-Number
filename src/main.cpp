525c81b8f88a51cf6c41425625ca1fb58dfbefe1
VikudouSennin <cao.d.vi@gmail.com>
2018-11-06 11:26:50 +0700

    Update src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "QINT.h"
+#include <string>
+#include <iostream>
+
+int main() {
+	QINT a,b,c;
+	a.input();
+	b.input();
+	
+
+	//c = a/b;
+	//c.output();
+	
+	cout << a.toDec();
+
+	system("pause");
+	return 1;
+}
